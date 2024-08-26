955d43016010b2400ff312b8ecd77909d74040d1
ahsan-io <muhammad.ahsan@hashpotato.io>
2024-08-26 13:16:10 +0500

    :)

---
+++ app/landing/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      {/* Your landing page content goes here */}
+      <h1>Welcome to My Awesome Landing Page</h1>
+      <p>This is a Next.js landing page built with TypeScript.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloPage: React.FC = () => {
+  return (
+    <div>
+      {/* Your landing page content goes here */}
+      <h1>HELLO!</h1>
+    </div>
+  );
+};
+
+export default HelloPage;
