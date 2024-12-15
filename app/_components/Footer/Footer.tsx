c410fa212fdc0b53a0ca3730eb052c1f3f6c9b03
Muhammad Ahsan <ahsanmuhammed45@gmail.com>
2024-12-15 05:44:35 +0500

    change footer

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -4,28 +4,9 @@ type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>
-      <div className="footer-container">
-        <div className="footer-content">
-          <div className="footer-column">
-            <h3>Get in touch</h3>
-            <p>
-              If you have any questions or would like to work together, please
-              feel free to reach out to me.
-            </p>
-            <a href="mailto:ahsanmuhammed45@gmail.com">Email me</a>
-          </div>
-          <div className="footer-column">
-            <h3>Follow me</h3>
-            <div className="social-links">
-              <a href="https://www.instagram.com/calledahsan/">
-                Instagram
-              </a>
-              <a href="https://www.facebook.com/calledahsan">Facebook</a>
-              <a href="https://www.linkedin.com/in/calledahsan/">LinkedIn</a>
-            </div>
-          </div>
-        </div>
+    <div className='footer-container'>
+      <div className='footer-content'>
+        2024 - 2025
       </div>
     </div>
   )
